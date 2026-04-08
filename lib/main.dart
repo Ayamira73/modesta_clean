efeb0acca164af8b74a0c92195a9fed7bf88e8ba
milenamarinova1 <s2410892019@fhooe.at>
2026-04-08 15:09:26 +0200

    I changed the title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class ModestaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Modesta',
+      title: 'Modestaaaa',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFFF7F1EB),
         fontFamily: 'Arial',
